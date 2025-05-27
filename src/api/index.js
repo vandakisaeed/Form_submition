dcd9b572c28730117321b94657eb4bbf8ebcbd32
Jorge Paul <jserranopaul@gmail.com>
2025-05-27 21:13:55 +0200

    Contact

--- src/api/index.js
+++ src/api/index.js
@@ -2,6 +2,13 @@ import z from 'zod/v4';
 
 const newsletterSchema = z.email('Invalid email address');
 
+const contactFormSchema = z.object({
+  firstName: z.string('First name needs to be a string').min(1, 'First name is required'),
+  lastName: z.string('Last name needs to be a string').min(1, 'Last name is required'),
+  email: z.email('Invalid email address'),
+  message: z.string('Message needs to be a string').min(1, 'Message is required')
+});
+
 export const registerNewsletter = async email => {
   await new Promise(resolve => setTimeout(resolve, 1000));
   const { data, error } = newsletterSchema.safeParse(email);
@@ -11,4 +18,16 @@ export const registerNewsletter = async email => {
   newsletterList.push(data);
   localStorage.setItem('newsletterList', JSON.stringify(newsletterList));
   return `Successfully registered ${data} to the newsletter!`;
+};
+
+export const sendContactForm = async ({ firstName, lastName, email, message }) => {
+  await new Promise(resolve => setTimeout(resolve, 1000));
+  const { data, error } = contactFormSchema.safeParse({
+    firstName,
+    lastName,
+    email,
+    message
+  });
+  if (error) throw new Error(z.prettifyError(error));
+  return `Thank you for your message, ${data.firstName}! We will get back to you soon.`;
 };
--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,5 +1,46 @@
+import { ErrorBoundary } from 'react-error-boundary';
+import { ErrorFallback } from '../components';
+
 const Contact = () => {
-  return <div>Contact</div>;
+  return (
+    <div className='flex flex-col items-center'>
+      <ErrorBoundary FallbackComponent={ErrorFallback}>
+        <form>
+          <fieldset className='fieldset bg-base-200 border-base-300 rounded-box w-lg border p-4'>
+            <legend className='fieldset-legend'>Contact Us</legend>
+            <label className='label'>First Name</label>
+            <input className='input w-full' name='firstName' placeholder='First Name' />
+            <label className='label'>Last Name</label>
+            <input className='input w-full' name='lastName' placeholder='Last Name' />
+            <label className='label'>Email</label>
+            <input className='input w-full' name='email' placeholder='Email' />
+            <label className='label'>Message</label>
+            <textarea
+              className='textarea w-full'
+              name='message'
+              placeholder='Your message'
+              rows={4}
+            />
+            <button className='btn btn-neutral mt-4'>Send</button>
+          </fieldset>
+        </form>
+      </ErrorBoundary>
+      <div className='mockup-code mt-5'>
+        <pre data-prefix='1'>
+          <code>- Import sendContactForm from 'src/api/index.js';</code>
+        </pre>
+        <pre data-prefix='2'>
+          <code>- Create an action for the form</code>
+        </pre>
+        <pre data-prefix='3'>
+          <code>- Within the action, pass the form fields to sendContactForm</code>
+        </pre>
+        <pre data-prefix='4'>
+          <code>- Disable the button and show "Sending" while the action is being executed</code>
+        </pre>
+      </div>
+    </div>
+  );
 };
 
 export default Contact;
